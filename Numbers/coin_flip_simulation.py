910dc1ef835e0c9c010955c2e59f74fc862fd4eb
Codetheist <56620151+Codetheist@users.noreply.github.com>
2021-02-11 12:45:57 -0500

    A simulator for a single coin flip.

---
+++ Numbers/coin_flip_simulation.py
@@ -0,0 +1,11 @@
+# Coin Flip Simulation - Write some code that simulates flipping a single coin however many
+# times the user decides.
+# The code should record the outcomes and count the number of tails and heads.
+# It then returns the tax plus the total cost with tax.
+# Author: Codetheist
+# Company: Codetheist, LLC
+# Email: codetheist@codetheist.com
+
+def coin_flip_simulator():
+    number_of_heads = 0
+    number_of_tails = 0
