eaf5eb64b02586d1bb6a82a8042dc4515e54a784
Codetheist <56620151+Codetheist@users.noreply.github.com>
2021-02-11 12:44:37 -0500

    A simple calculator to do basic operators.

---
+++ Numbers/calculator.py
@@ -0,0 +1,9 @@
+# Calculator - A simple calculator to do basic operators.
+# Make it a scientific calculator for added complexity.
+# It then returns the tax plus the total cost with tax.
+# Author: Codetheist
+# Company: Codetheist, LLC
+# Email: codetheist@codetheist.com
+
+print("Which type of calculator do you want to use?\n")
+print("")
