9df9d4e21bd482100a724676a1f041e210e425c4
Codetheist <56620151+Codetheist@users.noreply.github.com>
2021-02-16 14:59:19 -0500

    A simple calculator to do basic operators.

--- Numbers/calculator.py
+++ Numbers/calculator.py
@@ -5,5 +5,50 @@
 # Company: Codetheist, LLC
 # Email: codetheist@codetheist.com
 
-print("Which type of calculator do you want to use?\n")
-print("")
+
+def calculator():
+    print("Which type of calculator do you want to use?\n")
+    print("1. Standard")
+    print("2. Scientific")
+    print("Q. Quit")
+
+    user_input = input("Select an option: ")
+    quit = "Q"
+    while user_input.upper() != quit:
+        if (user_input == "1"):
+            standard_calculator()
+            print("\nWhich type of calculator do you want to use?\n")
+            print("1. Standard")
+            print("2. Scientific")
+            print("Q. Quit")
+            user_input = input("Select an option: \n")
+        # elif (user_input == 2):
+            # scientific_calculator()
+
+
+def standard_calculator():
+
+    num1 = float(input("Enter the first number: "))
+    operator = input("Operator: ")
+    num2 = float(input("Enter the second number: "))
+    solution = 0.0
+
+    if operator == "+":
+        solution = num1 + num2
+    elif operator == "-":
+        solution = num1 - num2
+    elif operator == "*":
+        solution = num1 * num2
+    elif operator == "/":
+        solution = num1 / num2
+    else:
+        print("Invalid operator. Please select a operator")
+
+    print(f"{num1} {operator} {num2} equals {solution}")
+
+
+# def scientific_calculator():
+    #
+
+
+calculator()
