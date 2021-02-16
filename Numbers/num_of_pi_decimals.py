a78336af1aafec7e05fe19b9df6f4e6635fbad54
Codetheist <56620151+Codetheist@users.noreply.github.com>
2021-02-16 13:05:06 -0500

    Find PI to the Nth Digit. The program generate PI up to that decimal places.

---
+++ Numbers/num_of_pi_decimals.py
@@ -0,0 +1,22 @@
+# Find PI to the Nth Digit - Enter a number and have the program generate PI up to that many decimal places.
+# Keep a limit to how far the program will go.
+# Author: Codetheist
+# Company: Codetheist, LLC
+# Email: codetheist@codetheist.com
+
+import math
+
+print("PI generator to the Nth digit\n")
+
+
+def pi_to_the_nth_digit():
+    num_digits = int(input("How many digits: "))
+
+    PI = math.pi
+
+    # pi_calc =
+
+    print(f"PI to the {num_digits} digit is amount: %.{num_digits}f")
+
+
+pi_to_the_nth_digit()
