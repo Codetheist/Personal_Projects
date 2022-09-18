51e64b05629b2f71e5e5295bd9a73612dc92c89b
Codetheist <56620151+Codetheist@users.noreply.github.com>
2022-09-18 17:48:41 -0400

    Tailwing config

--- Website/movie/tailwind.config.js
+++ Website/movie/tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/js/*.js",
+    "./src/pages/*.js",
+    "./index.html",
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
 }
