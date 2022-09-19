2c926e06cbca9b5bdc678155f6d9f6aa86d20142
Codetheist <56620151+Codetheist@users.noreply.github.com>
2022-09-19 17:14:21 -0400

    Update script.js

---
+++ Website/movie/src/js/script.js
@@ -0,0 +1,49 @@
+// Getting ID elements
+
+// TV Show
+let tvBackgroundImage = document.getElementById("background-img");
+let tvTitle = document.getElementById("tvshow-title");
+let tvDescription = document.getElementById("tvshow-description");
+
+
+// Movie & New Release
+let movieBackgroundImage = document.getElementById("background-image");
+let movieTitle = document.getElementById("title");
+let movieGenre = document.getElementById("genre");
+let movieRelease = document.getElementById("release-date");
+let movieTime = document.getElementById("time");
+
+// Search Result
+let searchBackgroundImage = document.getElementById("search-bg-img");
+let searchResultTitle = document.getElementById("search-title");
+let searchResultGenre = document.getElementById("search-description");
+let searchBar = document.getElementById("search-bar");
+
+// Get data from API
+let tvShowData = [];
+let movieData = [];
+let newReleaseData = [];
+let searchResultData = [];
+
+// API path
+
+
+// Creating a function to get the movie details
+function getMovieDetails() {
+    
+}
+
+// Creating a function to get the tv details
+function getTvShowDetails() {
+    
+}
+
+// Creating a function to get new release details
+function getNewReleaseDetails() {
+    
+}
+
+// Creating a function to get search result details
+function getSearchResultDetails() {
+    
+}
