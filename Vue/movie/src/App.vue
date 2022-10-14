b022d858672b5ef0bd750ae9235d8227acec7180
Codetheist <56620151+Codetheist@users.noreply.github.com>
2022-10-14 16:23:18 -0400

    Initialize movie app using Vue

---
+++ Vue/movie/src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+// This starter template is using Vue 3 < setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+//import Helloworld from './components/Helloworld.vue'
+import { ref } from 'vue'
+</script>
+
+<template>
+<!--<Helloworld msg="Vite + Vue" />-->
+</template>
