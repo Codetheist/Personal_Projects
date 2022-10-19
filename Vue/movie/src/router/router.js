ef1c262077e4cc8f19927b3bb6f89f24e0cf7f8f
Codetheist <56620151+Codetheist@users.noreply.github.com>
2022-10-19 13:10:30 -0400

    Update router.js

--- Vue/movie/src/router/router.js
+++ Vue/movie/src/router/router.js
@@ -1,4 +1,9 @@
 import { createWebHistory, createRouter } from 'vue-router';
+import Home from '../views/Home.vue';
+import Movies from '../views/Movies.vue';
+import Shows from '../views/Shows.vue';
+import Release from '../views/Release.vue';
+import Search from '../views/Search.vue';
 
 
 const routes = [
@@ -7,10 +12,30 @@ const routes = [
         name: 'Home',
         component: Home,
     },
+    {
+        path: '/movies',
+        name: 'Movies',
+        views: Movies,
+    },
+    {
+        path: '/shows',
+        name: 'Shows',
+        views: Shows,
+    },
+    {
+        path: '/search',
+        name: 'Search',
+        views: Search,
+    },
+    {
+        path: '/release',
+        name: 'Release',
+        views: Release,
+    },
 ];
 
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHistory(process.env.BASE_URL),
     routes,
 });
 
