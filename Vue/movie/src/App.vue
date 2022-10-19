b449f41e2357ec38f422f4aca96c820ed2f170fe
Codetheist <56620151+Codetheist@users.noreply.github.com>
2022-10-18 22:02:29 -0400

    Update App.vue

--- Vue/movie/src/App.vue
+++ Vue/movie/src/App.vue
@@ -1,10 +1,18 @@
 <script setup>
 // This starter template is using Vue 3 < setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import Personal from './components/Personal.vue'
+import Home from './views/Home.vue'
+import Movies from './views/Movies.vue'
+import Release from './views/Release.vue'
+import Shows from './views/Shows.vue'
+import Search from './views/Search.vue'
 import { ref } from 'vue'
 </script>
 
 <template>
-    <Personal msg="Personal Favorite" />
+    <Home msg="Personal Favorite" />
+    <Movies msg="Movies" />
+    <Release msg="Release" />
+    <Shows msg="Shows" />
+    <Search msg="Search Results" />
 </template>
