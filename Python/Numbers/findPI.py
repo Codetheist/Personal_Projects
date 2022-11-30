f162b3d28a503e0fe2dab266c87ab627738e2aae
Codetheist <56620151+Codetheist@users.noreply.github.com>
2022-11-29 20:30:17 -0500

    Update findPI.py
    Converted all print lines into f strings, added a limiter on the number of places, also correct the ordinal placement of the number.

--- Python/Numbers/findPI.py
+++ Python/Numbers/findPI.py
@@ -9,37 +9,63 @@ Description: This program finds PI to the Nth Digit
 import math
 
 # Print title
-print("****************************")
-print("* Find PI to the Nth Digit *")
-print("****************************\n")
+print(f"****************************")
+print(f"* Find PI to the Nth Digit *")
+print(f"****************************")
 
 # Creating a function to find PI to the Nth Digit
+
+
 def findPI(n):
-    # Finding PI to the Nth Digit
-    pi = round(math.pi, n)
-    # Returning the value of PI
-    return pi
+    # Creating an if statement to check if the user entered a number greater than 20
+    if n > 20:
+        # Printing an error message
+        print(f"\nError: The number you entered is greater than 20")
+    # Creating an else statement to check if the user entered a number less than or equal to 20
+    else:
+        # Finding PI to the Nth Digit
+        pi = round(math.pi, n)
+        # Returning the value of PI
+        return pi
 
 # Creating a function to print the value of PI
+
+
 def printPI(n):
-    # Printing the value of PI
-    print("The value of PI to the", n, "th digit is", findPI(n))
+    # If the number ends in 1, print st
+    if n % 10 == 1 and n != 10:
+        print(f"\nThe value of PI to the {n}st digit is {findPI(n)}")
+    # If the number ends in 2, print nd
+    elif n % 10 == 2:
+        print(f"\nThe value of PI to the {n}nd digit is {findPI(n)}")
+    # If the number ends in 3, print rd
+    elif n % 10 == 3:
+        print(f"\nThe value of PI to the {n}rd digit is {findPI(n)}")
+    else:
+        # Printing the value of PI
+        print(f"\nThe value of PI to the {n}th digit is {findPI(n)}")
 
 # Creating a function to get the value of N
+
+
 def getN():
     # Getting the value of N
-    n = int(input("Enter the value of N: "))
+    n = int(input(f"\nEnter the value of N: "))
     # Returning the value of N
     return n
 
 # Creating a function to check if the user wants to continue
+
+
 def checkContinue():
     # Getting the user's response
-    response = input("Do you want to continue? (Y/N): ")
+    response = input(f"\nDo you want to continue? (Y/N): ")
     # Returning the user's response
     return response
 
 # Run the program
+
+
 def main():
     # Creating a variable to ask if the user wants to continue
     continueProgram = "Y"
@@ -54,7 +80,7 @@ def main():
         # Creating an if statement to check if the user wants to continue
         if continueProgram.capitalize() == "N":
             # Printing a goodbye message
-            print("Goodbye!")
+            print(f"\nGoodbye!")
 
 
 # Calling the main function
