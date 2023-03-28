ffbdb65feb3fa5190768ef8a4811a810bb16e77d
Codetheist <56620151+Codetheist@users.noreply.github.com>
2023-03-28 15:25:09 -0400

    Update bandNameGenerator.py

--- Python/100 Days Code Challenge/bandNameGenerator.py
+++ Python/100 Days Code Challenge/bandNameGenerator.py
@@ -4,4 +4,4 @@
 print(f"Welcome to the Band Name Generator.")
 city = input(f"What's name of the city you grew up in?\n")
 pet = input(f"What's your pet's name?\n")
-print(f"Your band name could be " + city + " " + pet)
+print(f"Your band name could be {city} {pet}")
