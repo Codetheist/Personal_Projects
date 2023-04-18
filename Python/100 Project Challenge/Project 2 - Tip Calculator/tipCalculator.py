fe8ba714c9fcdc1d19c09cbdf03edbec287d3d32
Codetheist <56620151+Codetheist@users.noreply.github.com>
2023-04-18 16:51:49 -0400

    Delete tipCalculator.py

--- Python/100 Project Challenge/Project 2 - Tip Calculator/tipCalculator.py
+++
@@ -1,9 +0,0 @@
-# 100 Days of OF Code: Python
-# Day 2 - Tip Calculator
-
-print(f"Welcome to the tip calculator.")
-total_bill = float(input(f"What was the total bill? $"))
-total_people = int(input(f"How many people to split the bill? "))
-tip = int(input(f"What percentage tip would you like to give? 10, 12, or 15? "))
-person_bill = (total_bill + (total_bill * (tip / 100))) / total_people
-print(f"Each person should pay: ${person_bill:.2f}")
